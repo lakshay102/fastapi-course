3522ebbec048e4fe86991b5f9966d0590115baf2
Lakshay Gupta <lakshayanil165@gmail.com>
2025-02-12 19:10:40 +0530

    CRUD operations done

--- main.py
+++ main.py
@@ -24,6 +24,10 @@ def find_post(id):
         if id == post["id"]:
             return post
 
+def find_index_post(id):
+    for i,p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 @app.get("/")
 def root():
@@ -62,3 +66,27 @@ def get_post(id: int, response: Response):
         # return {"message" : f"post with id: {id} was not found"}
     # print(type(id))
     return {"post_detail": post}
+
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"Posts with id: {id} does not exist")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id : int, post: Post):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"Posts with id: {id} does not exist")
+    
+    post_dict = post.model_dump()
+
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data" : post_dict}
+    # print(post)
+    # return { "message" : "updated post"}
